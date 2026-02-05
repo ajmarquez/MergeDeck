a73b7be60bdb7eaf4dfe41dc47eb20dae4ff8900
Abelardo Marquez <abelardo.marquez@scout24.com>
2026-02-05 16:24:52 +0100

    Initial Commit

---
+++ MergeDeck/MergeDeckApp.swift
@@ -0,0 +1,17 @@
+//
+//  MergeDeckApp.swift
+//  MergeDeck
+//
+//  Created by Abelardo Jesus Marquez Gonzalez on 05.02.26.
+//
+
+import SwiftUI
+
+@main
+struct MergeDeckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MergeDeckTests/MergeDeckTests.swift
@@ -0,0 +1,16 @@
+//
+//  MergeDeckTests.swift
+//  MergeDeckTests
+//
+//  Created by Abelardo Jesus Marquez Gonzalez on 05.02.26.
+//
+
+import Testing
+
+struct MergeDeckTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
